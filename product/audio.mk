cc72a1cff15a050cc45eacbc888b8dd3b6cf2a31
PythonLimited <leonardomeitz@gmail.com>
2019-05-14 21:53:22 +0200

    add liblight and audio

--- product/audio.mk
+++ product/audio.mk
@@ -15,9 +15,13 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/tfa9897/seltech_bottom.ini:/system/etc/tfa9897/seltech_bottom.ini \
     $(LOCAL_PATH)/audio/tfa9897/seltech_bottom.cnt:/system/etc/tfa9897/seltech_bottom.cnt \
     $(LOCAL_PATH)/audio/tfa9897/seltech_stereo.cnt:/system/etc/tfa9897/seltech_stereo.cnt \
-    $(LOCAL_PATH)/audio/:tfa9897/seltech_top.cnt/system/etc/tfa9897/seltech_top.cnt \
+    $(LOCAL_PATH)/audio/tfa9897/seltech_top.cnt:/system/etc/tfa9897/seltech_top.cnt \
     $(LOCAL_PATH)/audio/tfa9897/seltech_top.ini:/system/etc/tfa9897/seltech_top.ini \
     $(LOCAL_PATH)/audio/tfa9897/seltech_stereo.ini:/system/etc/tfa9897/seltech_stereo.ini \
     $(LOCAL_PATH)/audio/tfa9897/LC/seltech_bottom.cnt:/system/etc/tfa9897/LC/seltech_bottom.cnt \
     $(LOCAL_PATH)/audio/tfa9897/LC/seltech_stereo.cnt:/system/etc/tfa9897/LC/seltech_stereo.cnt \
-    $(LOCAL_PATH)/audio/tfa9897/LC/seltech_top.cnt:/system/etc/tfa9897/LC/seltech_top.cnt
+    $(LOCAL_PATH)/audio/tfa9897/LC/seltech_top.cnt:/system/etc/tfa9897/LC/seltech_top.cnt \
+    $(LOCAL_PATH)/configs/mixer_paths_mtp.xml:/system/etc/mixer_paths_mtp.xml \
+    $(LOCAL_PATH)/configs/media_profiles.xml:/system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/configs/media_codecs_performance.xml:/system/etc/media_codecs_performance.xml \
+    $(LOCAL_PATH)/configs/media_codecs.xml:/system/etc/media_codecs.xml
