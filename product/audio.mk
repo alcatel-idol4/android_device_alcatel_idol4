44ae1252bda11d5396d15ffb991f255c4ebc3a41
PythonLimited <leonardomeitz@gmail.com>
2019-05-16 20:58:20 +0200

    copy tfa 9890 instead of 9897 (blame me for that)

--- product/audio.mk
+++ product/audio.mk
@@ -9,18 +9,15 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:system/etc/permissions/android.software.midi.xml
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/tfa9897/AAC/seltech_bottom.cnt:/system/etc/tfa9897/AAC/seltech_bottom.cnt \
-    $(LOCAL_PATH)/audio/tfa9897/AAC/seltech_stereo.cnt:/system/etc/tfa9897/AAC/seltech_stereo.cnt \
-    $(LOCAL_PATH)/audio/tfa9897/AAC/seltech_top.cnt:/system/etc/tfa9897/AAC/seltech_top.cnt \
-    $(LOCAL_PATH)/audio/tfa9897/seltech_bottom.ini:/system/etc/tfa9897/seltech_bottom.ini \
-    $(LOCAL_PATH)/audio/tfa9897/seltech_bottom.cnt:/system/etc/tfa9897/seltech_bottom.cnt \
-    $(LOCAL_PATH)/audio/tfa9897/seltech_stereo.cnt:/system/etc/tfa9897/seltech_stereo.cnt \
-    $(LOCAL_PATH)/audio/tfa9897/seltech_top.cnt:/system/etc/tfa9897/seltech_top.cnt \
-    $(LOCAL_PATH)/audio/tfa9897/seltech_top.ini:/system/etc/tfa9897/seltech_top.ini \
-    $(LOCAL_PATH)/audio/tfa9897/seltech_stereo.ini:/system/etc/tfa9897/seltech_stereo.ini \
-    $(LOCAL_PATH)/audio/tfa9897/LC/seltech_bottom.cnt:/system/etc/tfa9897/LC/seltech_bottom.cnt \
-    $(LOCAL_PATH)/audio/tfa9897/LC/seltech_stereo.cnt:/system/etc/tfa9897/LC/seltech_stereo.cnt \
-    $(LOCAL_PATH)/audio/tfa9897/LC/seltech_top.cnt:/system/etc/tfa9897/LC/seltech_top.cnt \
+    $(LOCAL_PATH)/audio/tfa9890/AAC/tct_btm.cnt:/system/etc/tfa9890/AAC/tct_btm.cnt \
+    $(LOCAL_PATH)/audio/tfa9890/AAC/tct_top.cnt:/system/etc/tfa9890/AAC/tct_top.cnt \
+    $(LOCAL_PATH)/audio/tfa9890/AAC/tct_stereo.cnt:/system/etc/tfa9890/AAC/tct_stereo.cnt \
+    $(LOCAL_PATH)/audio/tfa9890/tct_btm.cnt:/system/etc/tfa9890/tct_btm.cnt \
+    $(LOCAL_PATH)/audio/tfa9890/tct_top.cnt:/system/etc/tfa9890/tct_top.cnt \
+    $(LOCAL_PATH)/audio/tfa9890/tct_stereo.cnt:/system/etc/tfa9890/tct_stereo.cnt \
+    $(LOCAL_PATH)/audio/tfa9890/LC/tct_btm.cnt:/system/etc/tfa9890/LC/tct_btm.cnt \
+    $(LOCAL_PATH)/audio/tfa9890/LC/tct_top.cnt:/system/etc/tfa9890/LC/tct_top.cnt \
+    $(LOCAL_PATH)/audio/tfa9890/LC/tct_stereo.cnt:/system/etc/tfa9890/LC/tct_stereo.cnt \
     $(LOCAL_PATH)/configs/mixer_paths_mtp.xml:/system/etc/mixer_paths_mtp.xml \
     $(LOCAL_PATH)/configs/media_profiles.xml:/system/etc/media_profiles.xml \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:/system/etc/media_codecs_performance.xml \
