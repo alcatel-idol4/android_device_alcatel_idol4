55097d575c5936a31d8775140cb56c4716a93d21
pythonlimited <leonardomeitz@gmail.com>
2019-08-24 19:59:59 +0200

    camera: give camera init fix its own service

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -46,6 +46,13 @@ LOCAL_SRC_FILES    := etc/init.qcom.wifi.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.camera.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.camera.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
